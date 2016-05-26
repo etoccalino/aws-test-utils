4d79d409ef7315fbf3fc0d8dd495eb3c1b14509c
Elvio Toccalino <me@etoccalino.com>
2016-05-26 19:02:39 -0300

    upgraded version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'awstestutils',
     packages = ['awstestutils'],
-    version = '0.1.6',
+    version = '0.2.0',
     description = 'Artifacts to test dependencies with AWS using boto3',
     license = 'BSD',
 
